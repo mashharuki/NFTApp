14fca67c9a0dd9e8f9af79054dae78369c531192
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-05-03 21:32:29 +0900

    ローカルにデプロイ完了

---
+++ elonNFT/contracts/ElonNFT.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0; 
+
+import "hardhat/console.sol"; 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract ElonNFT is ERC721URIStorage { 
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+	
+    constructor() ERC721("ElonNFT", "ELON") {
+        console.log("This is my Elon Musk NFT contract!!");
+    }
+
+    function mintNFT() public returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/JJJS");
+        
+        console.log("The NFT ID %s has been minted to %s", newItemId, msg.sender);
+        return newItemId;
+    }
+}
